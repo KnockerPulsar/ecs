4e5e370446799adbd1da21407a077fc2b6526585
Tarek Yasser <tarekspeed7@gmail.com>
2023-11-20 04:32:54 +0200

    Move iterator code to its own class.

--- src/ecs.cpp
+++ src/ecs.cpp
@@ -10,11 +10,79 @@
 #include <utility>
 #include <vector>
 
+struct Foo {
+  int x = 42;
+};
+
+struct Bar {
+  std::string y = "bar";
+};
+
 namespace ecs {
 using Entity = std::uint32_t;
 
+template <typename T> using Iterator = typename std::vector<T>::iterator;
+
 class ECS {
   std::unordered_map<std::type_index, std::any> component_vectors;
+
+  template <typename... Ts> struct MultiIterator {
+    ECS &owner;
+    std::tuple<Iterator<Ts>...> begins, ends;
+
+    MultiIterator(ECS &o)
+        : owner(o), begins(getBegins(std::tuple<Ts...>{})),
+          ends(getEnds(std::tuple<Ts...>{})) {}
+
+    template <typename T2, typename... Ts2>
+    auto getBegins(std::tuple<T2, Ts2...>) {
+      auto t_begin = std::make_tuple(owner.getComponentVector<T2>().begin());
+
+      if constexpr (sizeof...(Ts2) > 0) {
+        auto remaining = getBegins(std::tuple<Ts2...>{});
+        return std::tuple_cat(t_begin, remaining);
+      } else
+        return t_begin;
+    }
+
+    template <typename T2, typename... Ts2>
+    auto getEnds(std::tuple<T2, Ts2...>) {
+      auto t_end = std::make_tuple(owner.getComponentVector<T2>().end());
+
+      if constexpr (sizeof...(Ts2) > 0) {
+        auto remaining = getEnds(std::tuple<Ts2...>{});
+        return std::tuple_cat(t_end, remaining);
+      } else
+        return t_end;
+    }
+
+    template <typename F> void forEach(F &&fn) {
+      auto current = begins;
+
+      while (current != ends) {
+        /* const auto all_some = std::apply( */
+        /*     [](auto &&...args) { return std::all_of(((args.has_value()),
+         * ...));
+         * }, */
+        /*     begin); */
+
+        /* if (all_some) { */
+        std::apply(fn, current);
+        /* } */
+
+        advance_all<sizeof...(Ts) - 1>(current);
+      }
+    }
+
+    template <unsigned int N, typename... Ts2>
+    void advance_all(std::tuple<Ts2...> &iters) {
+      std::get<N>(iters)++;
+
+      if constexpr (N >= 1) {
+        advance_all<N - 1, Ts2...>(iters);
+      }
+    }
+  };
 
 public:
   template <typename... Ts> Entity addEntity(Ts &&...comps) {
@@ -39,98 +107,12 @@ public:
     }
   }
 
-  template <typename T> typename std::vector<T>::iterator components_begin() {
-    return getComponentVector<T>().begin();
-  }
-
-  template <typename T> typename std::vector<T>::iterator components_end() {
-    return getComponentVector<T>().end();
-  }
-
-  template <typename T, typename... Ts> auto getBegins(std::tuple<T, Ts...>) {
-    if constexpr (sizeof...(Ts) > 0) {
-      auto remaining = getBegins(std::tuple<Ts...>{});
-      return std::tuple_cat(std::make_tuple(components_begin<T>()), remaining);
-    } else
-      return std::make_tuple(components_begin<T>());
-  }
-
-  // Wrapper function to initialize the recursive call
-  template <typename... Ts> auto _begin() {
-    return getBegins(std::tuple<Ts...>{});
-  }
-
-  // Recursive case: handle the first type and recursively call for the
-  // remaining types
-  template <typename T, typename... Ts> auto getEnds(std::tuple<T, Ts...>) {
-    if constexpr (sizeof...(Ts) > 0) {
-      auto remaining = getEnds(std::tuple<Ts...>{});
-      return std::tuple_cat(std::make_tuple(components_end<T>()), remaining);
-    } else
-      return std::make_tuple(components_end<T>());
-  }
-
-  // Wrapper function to initialize the recursive call
-  template <typename... Ts> auto _end() { return getEnds(std::tuple<Ts...>{}); }
-
-  /* template <typename T, typename... Ts> */
-  /* auto unwrapIterators(std::tuple<T, Ts...> iterTuple) { */
-  /*   const auto t = std::make_tuple(*std::get<T>(iterTuple)); */
-  /*   if constexpr (sizeof...(Ts) > 0) { */
-  /*     return std::tuple_cat(t, unwrapIterators<Ts...>(iterTuple)); */
-  /*   } else { */
-  /*     return t; */
-  /*   } */
-  /* } */
-
   template <typename... Ts, typename F> void forEach(F &&fn) {
-    auto begin = _begin<Ts...>();
-    auto end = _end<Ts...>();
-
-    while (begin != end) {
-      /* const auto all_some = std::apply( */
-      /*     [](auto &&...args) { return std::all_of(((args.has_value()), ...));
-       * }, */
-      /*     begin); */
-
-      /* if (all_some) { */
-      std::apply(fn, begin);
-      /* } */
-
-      advance_all<sizeof...(Ts) - 1>(begin);
-    }
+    MultiIterator<Ts...>(*this).forEach(fn);
   }
-
-  // Recursive case: handle the first type and recursively call for the
-  // remaining types
-  template <unsigned int N, typename T, typename... Ts>
-  void advance_all(std::tuple<T, Ts...> &iters) {
-    std::get<N>(iters)++;
-
-    if constexpr (N >= 1) {
-      advance_all<N - 1, T, Ts...>(iters);
-    }
-  }
-};
-
-template <typename... Ts> struct Query {
-  std::tuple<Ts...> beginnings, ends;
-
-  auto begin() { return beginnings; }
-  auto end() { return ends; }
 };
 
 } // namespace ecs
-
-struct Foo {
-  int x = 42;
-};
-
-struct Bar {
-  std::string y = "bar";
-};
-
-template <typename T> using Iterator = typename std::vector<T>::iterator;
 
 int main() {
   ecs::ECS ecs;
@@ -138,11 +120,8 @@ int main() {
   const auto t1 = ecs.addEntity(Foo{}, Bar{});
   const auto t2 = ecs.addEntity(Foo{69}, Bar{"urmom"});
 
-  ecs.forEach<Foo>([](auto f) {
-    std::cout << f->x << " "
-              << "" << '\n';
-
-    f->x *= 2;
+  ecs.forEach<Foo, Bar>([](ecs::Iterator<Foo> f, ecs::Iterator<Bar> b) {
+    std::cout << f->x << " " << b->y << '\n';
   });
 
   return 0;
