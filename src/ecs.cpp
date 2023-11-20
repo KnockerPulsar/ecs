4078854113caea9da4212d0d4ef5970f352133cb
Tarek Yasser <tarekspeed7@gmail.com>
2023-11-20 19:11:35 +0200

    Apply formatting to ecs code.

--- src/ecs.cpp
+++ src/ecs.cpp
@@ -28,15 +28,18 @@ struct Baz {
 namespace ecs {
 using Entity = std::uint32_t;
 
-template <typename T> using Vector = typename std::vector<std::optional<T>>;
-template <typename T> using OptIter = typename Vector<T>::iterator;
-template <typename T> using Iter = typename std::vector<T>::iterator;
+template <typename T>
+using Vector = typename std::vector<std::optional<T>>;
+template <typename T>
+using OptIter = typename Vector<T>::iterator;
+template <typename T>
+using Iter = typename std::vector<T>::iterator;
 
 struct ComponentContainer {
   struct ComponentOperation {
-    std::function<void()> pushDummy;
+    std::function<void()>                                     pushDummy;
     std::function<void(ComponentContainer &, Entity, Entity)> moveComponent;
-    std::function<void(Entity)> removeComponent;
+    std::function<void(Entity)>                               removeComponent;
   };
 
   friend class ECS;
@@ -48,14 +51,16 @@ struct ComponentContainer {
 
   uint32_t numEntities = 0;
 
-  template <typename T> Vector<T> &getComponentVector() {
+  template <typename T>
+  Vector<T> &getComponentVector() {
     // NOTE: map::operator[] default constructs at whatever key you're looking
     // up if there's no value there. In this case, it constructs an empty
     // `std::any`
     return std::any_cast<Vector<T> &>(component_vectors[typeid(T)]);
   }
 
-  template <typename... Ts> Entity addEntity(Ts &&...comps) {
+  template <typename... Ts>
+  Entity addEntity(Ts &&...comps) {
     (lazilyInitComponentVector<Ts>(), ...);
     (lazilyRegisterComponentOperations<Ts>(), ...);
 
@@ -70,47 +75,43 @@ struct ComponentContainer {
   }
 
 private:
-  template <typename T> void addComponent(T comp) {
+  template <typename T>
+  void addComponent(T comp) {
     getComponentVector<T>()[numEntities] = comp;
   }
 
-  template <typename T> void lazilyInitComponentVector() {
+  template <typename T>
+  void lazilyInitComponentVector() {
     auto &vec_any = component_vectors[typeid(T)];
     if (!vec_any.has_value()) {
       vec_any = std::make_any<Vector<T>>(Vector<T>(numEntities, std::nullopt));
     }
   }
 
-  template <typename T> void lazilyRegisterComponentOperations() {
-    if (auto f = componentOperations.find(typeid(T));
-        f == componentOperations.end()) {
-      auto dummyPusher = [this]() {
-        getComponentVector<T>().push_back(std::nullopt);
-      };
+  template <typename T>
+  void lazilyRegisterComponentOperations() {
+    if (auto f = componentOperations.find(typeid(T)); f == componentOperations.end()) {
+      auto dummyPusher = [this]() { getComponentVector<T>().push_back(std::nullopt); };
 
-      auto moveComponent = [this](ComponentContainer &oth, Entity sourceId,
-                                  Entity destId) {
-        auto &selfVec = getComponentVector<T>();
-        auto &otherVec = oth.getComponentVector<T>();
+      auto moveComponent = [this](ComponentContainer &oth, Entity sourceId, Entity destId) {
+        auto &selfVec    = getComponentVector<T>();
+        auto &otherVec   = oth.getComponentVector<T>();
         otherVec[destId] = std::move(selfVec[sourceId]);
       };
 
-      auto removeComponent = [this](Entity eid) {
-        getComponentVector<T>()[eid] = std::nullopt;
-      };
+      auto removeComponent = [this](Entity eid) { getComponentVector<T>()[eid] = std::nullopt; };
 
-      componentOperations.insert(
-          {typeid(T),
-           ComponentOperation{dummyPusher, moveComponent, removeComponent}});
+      componentOperations.insert({typeid(T), ComponentOperation{dummyPusher, moveComponent, removeComponent}});
     }
   }
 };
 
 struct Commands {
-  ComponentContainer entitiesToAdd;
+  ComponentContainer  entitiesToAdd;
   std::vector<Entity> entitiesToRemove;
 
-  template <typename... Ts> void addEntity(Ts &&...comps) {
+  template <typename... Ts>
+  void addEntity(Ts &&...comps) {
     entitiesToAdd.addEntity(std::forward<Ts>(comps)...);
   }
 
@@ -118,13 +119,12 @@ struct Commands {
 };
 
 class ECS {
-  template <typename... Ts> struct MultiIterator {
-    ECS &owner;
+  template <typename... Ts>
+  struct MultiIterator {
+    ECS                       &owner;
     std::tuple<OptIter<Ts>...> begins, ends;
 
-    MultiIterator(ECS &o)
-        : owner(o), begins(getBegins(std::tuple<Ts...>{})),
-          ends(getEnds(std::tuple<Ts...>{})) {}
+    MultiIterator(ECS &o) : owner(o), begins(getBegins(std::tuple<Ts...>{})), ends(getEnds(std::tuple<Ts...>{})) {}
 
     template <typename T2, typename... Ts2>
     auto getBegins(std::tuple<T2, Ts2...>) {
@@ -151,7 +151,7 @@ class ECS {
     template <unsigned int N = 0, unsigned int NEnd = sizeof...(Ts) - 1>
     auto unwrapIterators(std::tuple<OptIter<Ts>...> iterTuple) {
       using T = typename std::tuple_element<N, std::tuple<Ts...>>::type;
-      Iter<T> comp_it(&(std::get<N>(iterTuple)->value()));
+      Iter<T>    comp_it(&(std::get<N>(iterTuple)->value()));
       const auto t = std::make_tuple(comp_it);
 
       if constexpr (N == NEnd) {
@@ -170,12 +170,11 @@ class ECS {
       }
     }
 
-    template <typename F> void forEach(F &&fn) {
+    template <typename F>
+    void forEach(F &&fn) {
 
-      for (auto current = begins; current != ends;
-           advance_all<sizeof...(Ts) - 1>(current)) {
-        const auto all_some = std::apply(
-            [](auto &&...args) { return (args->has_value() && ...); }, current);
+      for (auto current = begins; current != ends; advance_all<sizeof...(Ts) - 1>(current)) {
+        const auto all_some = std::apply([](auto &&...args) { return (args->has_value() && ...); }, current);
 
         if (all_some) {
           std::apply(fn, unwrapIterators(current));
@@ -183,37 +182,32 @@ class ECS {
       }
     }
 
-    template <typename F> void forEachMut(F &&fn, Commands &cmd) {
+    template <typename F>
+    void forEachMut(F &&fn, Commands &cmd) {
 
-      for (auto current = begins; current != ends;
-           advance_all<sizeof...(Ts) - 1>(current)) {
+      for (auto current = begins; current != ends; advance_all<sizeof...(Ts) - 1>(current)) {
 
-        const auto all_some = std::apply(
-            [](auto &&...args) { return (args->has_value() && ...); }, current);
+        const auto all_some = std::apply([](auto &&...args) { return (args->has_value() && ...); }, current);
 
         if (all_some) {
-          auto paramTuple = std::tuple_cat(std::make_tuple(std::ref(cmd)),
-                                           unwrapIterators(current));
+          auto paramTuple = std::tuple_cat(std::make_tuple(std::ref(cmd)), unwrapIterators(current));
           std::apply(fn, paramTuple);
         }
       }
     }
 
-    template <typename F> void forEachMutId(F &&fn, Commands &cmd) {
+    template <typename F>
+    void forEachMutId(F &&fn, Commands &cmd) {
 
       auto multiIter = begins;
-      auto eid = 0;
-      for (; multiIter != ends;
-           advance_all<sizeof...(Ts) - 1>(multiIter), eid++) {
+      auto eid       = 0;
+      for (; multiIter != ends; advance_all<sizeof...(Ts) - 1>(multiIter), eid++) {
 
-        const auto all_some = std::apply(
-            [](auto &&...args) { return (args->has_value() && ...); },
-            multiIter);
+        const auto all_some = std::apply([](auto &&...args) { return (args->has_value() && ...); }, multiIter);
 
         if (all_some) {
-          auto paramTuple = std::tuple_cat(std::make_tuple(eid),
-                                           std::make_tuple(std::ref(cmd)),
-                                           unwrapIterators(multiIter));
+          auto paramTuple =
+              std::tuple_cat(std::make_tuple(eid), std::make_tuple(std::ref(cmd)), unwrapIterators(multiIter));
           std::apply(fn, paramTuple);
         }
       }
@@ -232,7 +226,8 @@ public:
     return components.numEntities;
   }
 
-  template <typename... Ts> Entity addEntity(Ts &&...comps) {
+  template <typename... Ts>
+  Entity addEntity(Ts &&...comps) {
     components.addEntity(std::forward<Ts>(comps)...);
 
     return components.numEntities;
@@ -244,26 +239,26 @@ public:
     }
   }
 
-  template <typename T> auto &getComponentVector() {
+  template <typename T>
+  auto &getComponentVector() {
     return components.getComponentVector<T>();
   }
 
-  template <typename... Ts, typename F> void forEach(F &&fn) {
+  template <typename... Ts, typename F>
+  void forEach(F &&fn) {
     MultiIterator<Ts...>(*this).forEach(fn);
   }
 
   void copyComponents(Commands &cmd, Entity sourceId, Entity destId) {
     for (auto &[typeId, anyVec] : cmd.entitiesToAdd.component_vectors) {
-      cmd.entitiesToAdd.componentOperations[typeId].moveComponent(
-          components, sourceId, destId);
+      cmd.entitiesToAdd.componentOperations[typeId].moveComponent(components, sourceId, destId);
     }
   }
 
   void processCommands(Commands &cmd) {
     // Loops over all added entities, creates an empty one in the ECS storage,
     // then moves over all components from `entitiesToAdd` to the ECS storage.
-    for (uint32_t sourceId = 0; sourceId < cmd.entitiesToAdd.numEntities;
-         sourceId++) {
+    for (uint32_t sourceId = 0; sourceId < cmd.entitiesToAdd.numEntities; sourceId++) {
       auto destId = addEmptyEntity();
       copyComponents(cmd, sourceId, destId);
     }
@@ -273,14 +268,16 @@ public:
     }
   }
 
-  template <typename... Ts, typename F> void forEachMut(F &&fn) {
+  template <typename... Ts, typename F>
+  void forEachMut(F &&fn) {
     Commands cmd{};
     MultiIterator<Ts...>(*this).forEachMut(fn, cmd);
 
     processCommands(cmd);
   }
 
-  template <typename... Ts, typename F> void forEachMutId(F &&fn) {
+  template <typename... Ts, typename F>
+  void forEachMutId(F &&fn) {
     Commands cmd{};
     MultiIterator<Ts...>(*this).forEachMutId(fn, cmd);
 
@@ -299,8 +296,7 @@ int main() {
   const auto t4 = ecs.addEntity(Baz{12.14});
   const auto t5 = ecs.addEntity(Foo{122}, Baz{12.14});
 
-  ecs.forEach<Foo, Baz>(
-      [](auto f, auto b) { std::cout << f->x << " " << b->z << '\n'; });
+  ecs.forEach<Foo, Baz>([](auto f, auto b) { std::cout << f->x << " " << b->z << '\n'; });
 
   std::cout << "===\n";
 
@@ -310,19 +306,16 @@ int main() {
     }
   });
 
-  ecs.forEach<Foo, Baz>(
-      [](auto f, auto b) { std::cout << f->x << " " << b->z << '\n'; });
+  ecs.forEach<Foo, Baz>([](auto f, auto b) { std::cout << f->x << " " << b->z << '\n'; });
 
   std::cout << "===\n";
 
-  ecs.forEachMutId<Foo, Baz>(
-      [](ecs::Entity selfId, ecs::Commands &cmd, auto f, auto b) {
-        if (b->z == 100) {
-          cmd.removeEntity(selfId);
-        }
-      });
+  ecs.forEachMutId<Foo, Baz>([](ecs::Entity selfId, ecs::Commands &cmd, auto f, auto b) {
+    if (b->z == 100) {
+      cmd.removeEntity(selfId);
+    }
+  });
 
-  ecs.forEach<Foo, Baz>(
-      [](auto f, auto b) { std::cout << f->x << " " << b->z << '\n'; });
+  ecs.forEach<Foo, Baz>([](auto f, auto b) { std::cout << f->x << " " << b->z << '\n'; });
   return 0;
 }
